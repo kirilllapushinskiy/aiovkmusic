1e820714c1df66d40397038078220c38ee18954c
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-28 22:20:44 +0300

    Have been set vk_api constants to improve performance.

--- source/aiovkmusic/session.py
+++ source/aiovkmusic/session.py
@@ -12,25 +12,31 @@ class VKSession:
         :param password: Пароль пользователя вконтакте.
         :param session_file_path: Путь к файлу для сохранения сессии.
         """
+        VkApi.RPS_DELAY = 0
         try:
-            self.__vk_session = VkApi(
+            self._vk_session = VkApi(
                 login=login,
                 password=password,
                 config_filename=session_file_path
             )
-            self.__vk_session.auth(token_only=True)
+            self._vk_session.auth(token_only=True)
         except AuthError as err:
             raise AuthorizationError() from err
         except Captcha:
             raise AwaitedCaptcha()
         try:
-            self.__vk_audio = audio.VkAudio(self.__vk_session)
+            audio.RPS_DELAY_RELOAD_AUDIO = 0
+            audio.RPS_DELAY_LOAD_SECTION = 0
+            audio.TRACKS_PER_USER_PAGE = 1000
+            audio.TRACKS_PER_ALBUM_PAGE = 1000
+            audio.ALBUMS_PER_USER_PAGE = 100
+            self.__vk_audio = audio.VkAudio(vk=self._vk_session)
         except ApiError as err:
             raise AuthorizationError() from err
 
     @property
     def api(self) -> VkApi:
-        return self.__vk_session
+        return self._vk_session
 
     @property
     def audio(self) -> audio.VkAudio:
